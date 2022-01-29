7656981d8a2a73452c86196a13af504be875041d
elviraff <elvvvirk@mail.ru>
2022-01-29 16:43:43 +0400

    part1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <header>
+      <div class="title">My personal costs</div>
+    </header>
+    <br>
+    <button @click="show=!show">ADD +</button>
+    <br>
+    <main>
+      <br>
+      <AddPaymentForm v-if="show" @add="add"/>
+      <button v-if="show" @click="show=!show">CLOSE</button>
+      <payments-display :items="paymentsList"/>
+    </main>
   </div>
 </template>
 
@@ -14,8 +26,50 @@
 //     HelloWorld
 //   }
 // }
+import PaymentsDisplay from "@/components/payments";
+import AddPaymentForm from "@/components/AddPaymentForm";
 export default {
-  name: ''
+  name: '',
+  components: {
+    AddPaymentForm,
+    PaymentsDisplay,
+  },
+  data(){
+    return {
+      show: false,
+      paymentsList:[],
+    }
+  },
+  methods: {
+    add (data){
+      this.paymentsList = [...this.paymentsList, data]
+    },
+    fetchData() {
+      return [
+        {
+          id: "1",
+          date: '20.01.2022',
+          category: 'Food',
+          value: 169,
+        },
+        {
+          id: "2",
+          date: '21.01.2022',
+          category: 'Transport',
+          value: 300,
+        },
+        {
+          id: "3",
+          date: '21.01.2022',
+          category: 'Food',
+          value: 800,
+        },
+      ]
+    }
+  },
+    created(){
+      this.paymentsList = this.fetchData()
+    }
 }
 </script>
 
